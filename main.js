6127e7af2d43f61c810dc05b8129a5915203a014
Dante Fuertes <150140918+DavidFuertes@users.noreply.github.com>
2024-07-21 21:01:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,34 @@ document.addEventListener("DOMContentLoaded", () => {
   const listaPokemon = document.querySelector("#listaPokemon");
   const botonesHeader = document.querySelectorAll(".btn-header");
   const URL = "https://pokeapi.co/api/v2/pokemon/";
+  const searchBar = document.querySelector("#search");
 
   // Cargar los primeros 151 Pokémon
   for (let i = 1; i <= 151; i++) {
     fetch(URL + i)
       .then((response) => response.json())
       .then((data) => mostrarPokemon(data))
-      .catch((error) => console.error("Error fetching Pokémon:", error));
+      .catch((error) => console.error("Error buscando al Pokémon:", error));
   }
+
+  // Función para el buscador de Pokémon por nombre o número
+  searchBar.addEventListener("input", (event) => {
+    const searchTerm = event.target.value.toLowerCase();
+    const pokemonList = document.querySelectorAll(".pokemon");
+    pokemonList.forEach((pokemon) => {
+      const pokemonName = pokemon
+        .querySelector(".pokemon-nombre")
+        .textContent.toLowerCase();
+      const pokemonId = pokemon
+        .querySelector(".pokemon-id")
+        .textContent.toLowerCase();
+      if (pokemonName.includes(searchTerm) || pokemonId.includes(searchTerm)) {
+        pokemon.style.display = "block";
+      } else {
+        pokemon.style.display = "none";
+      }
+    });
+  });
 
   // Función para mostrar Pokémon
   function mostrarPokemon(poke) {
@@ -19,39 +39,39 @@ document.addEventListener("DOMContentLoaded", () => {
     const pokeTypes = poke.types
       .map(
         (type) => `
-          <img src="./img/type/Tipo_${type.type.name}.webp" alt="${type.type.name}" class="tipo">
-        `
+            <img src="./img/type/Tipo_${type.type.name}.webp" alt="${type.type.name}" class="tipo">
+          `
       )
       .join("");
 
     div.innerHTML = `
-          <div class="pokemon">
-            <p class="pokemon-id-back">#${poke.id
-              .toString()
-              .padStart(3, "0")}</p>
-            <div class="pokemon-image">
-              <img
-                src="${poke.sprites.other["official-artwork"].front_default}"
-                alt="${poke.name}"
-              />
+            <div class="pokemon">
+              <p class="pokemon-id-back">#${poke.id
+                .toString()
+                .padStart(3, "0")}</p>
+              <div class="pokemon-image">
+                <img
+                  src="${poke.sprites.other["official-artwork"].front_default}"
+                  alt="${poke.name}"
+                />
+              </div>
+              <div class="pokemon-info">
+                <div class="nombre-contenedor">
+                  <p class="pokemon-id">#${poke.id
+                    .toString()
+                    .padStart(3, "0")}</p>
+                  <h2 class="pokemon-nombre">${poke.name}</h2>
+                </div>
+                <div class="pokemon-tipos">
+                  ${pokeTypes}
+                </div>
+                <div class="pokemon-stats">
+                  <p class="stat">Altura: ${(poke.height / 10).toFixed(1)} m</p>
+                  <p class="stat">Peso: ${(poke.weight / 10).toFixed(1)} kg</p>
+                </div>
+              </div>
             </div>
-            <div class="pokemon-info">
-              <div class="nombre-contenedor">
-                <p class="pokemon-id">#${poke.id
-                  .toString()
-                  .padStart(3, "0")}</p>
-                <h2 class="pokemon-nombre">${poke.name}</h2>
-              </div>
-              <div class="pokemon-tipos">
-                ${pokeTypes}
-              </div>
-              <div class="pokemon-stats">
-                <p class="stat">Altura: ${(poke.height / 10).toFixed(1)} m</p>
-                <p class="stat">Peso: ${(poke.weight / 10).toFixed(1)} kg</p>
-              </div>
-            </div>
-          </div>
-        `;
+          `;
 
     listaPokemon.appendChild(div);
   }
